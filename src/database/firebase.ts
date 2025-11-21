72bbce49e6399a89f2729c0d9a3d7acf977eb268
Devi Manikanta <manikantapalaparthi@gmail.com>
2025-11-21 15:42:16 +0530

    use env for firestore details

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -1,9 +1,17 @@
-var admin = require("firebase-admin");
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-var serviceAccount = require("./config.json");
+dotenv.config();
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT environment variable");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
